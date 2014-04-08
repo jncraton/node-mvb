0274b8950e680db3aa6cc8206f60373015253b0f
Jon Craton <jncraton@gmail.com>
2014-04-08 14:40:01 -0400

    fixed broken slug to id lookup logic

--- mvb.js
+++ mvb.js
@@ -179,11 +179,11 @@ app.get('/:parent/:id', function(req, res){
         res.redirect(301, '/' + req.params.parent + '/' + req.params.id + '/' + pages[req.params.parent][req.params.id].slug + '/');
     } else {
         // Assume that the form is /:parent/:slug and redirect
-        
+
         Object.keys(pages[req.params.parent]).forEach(function (id) {
-            if (!isNaN(id)) {
-                var slug = pages[req.params.parent][id].slug
-                
+            var slug = pages[req.params.parent][id].slug
+            
+            if (!isNaN(id) && slug == req.params.id) {
                 res.redirect(301, '/' + req.params.parent + '/' + id + '/' + slug + '/');
             }
         });
